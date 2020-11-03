dfdfb73b66b142810299c07278a877c0e766380c
NiNoWorkout <ninoworkout@gmail.com>
2020-11-03 17:29:38 +0300

    start

--- Ess_10_1/Program.cs
+++ Ess_10_1/Program.cs
@@ -6,23 +6,11 @@ using System.Threading.Tasks;
 
 namespace Ess_10_1
 {
-    class MyClass<T> where T : new()
-    {
-        public static T FactoryMethod()
-        {
-            return new T();
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            int a = MyClass<int>.FactoryMethod();
-            Console.WriteLine(a.GetType().Name);
 
-            Program p = MyClass<Program>.FactoryMethod();
-            Console.WriteLine(p.GetType().Name);
 
             // Delay.
             Console.ReadKey();
