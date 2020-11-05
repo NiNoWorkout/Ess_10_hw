acdb96bba841b2ea531defb253876c8f10377d5f
NiNoWorkout <ninoworkout@gmail.com>
2020-11-05 10:45:40 +0300

    First tasks done

--- Ess_10_1/Program.cs
+++ Ess_10_1/Program.cs
@@ -6,11 +6,23 @@ using System.Threading.Tasks;
 
 namespace Ess_10_1
 {
+    class MyClass<T> where T : new()
+    {
+        public static T FactoryMethod()
+        {
+            return new T();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            int i = MyClass<int>.FactoryMethod();
+            Console.WriteLine(i);
 
+            char s = MyClass<char>.FactoryMethod();
+            Console.WriteLine(i);
 
             // Delay.
             Console.ReadKey();
